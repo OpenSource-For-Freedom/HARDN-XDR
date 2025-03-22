734cf791e10e0ae2583b6b6e68946768fb6fa067
Răduică Iulian <raduicaiulian@gmail.com>
2025-03-22 16:32:51 +0200

    added an if statement to skip installation of docker-compose when it is already installed and it is executable

--- setup/Setup.sh
+++ setup/Setup.sh
@@ -40,9 +40,13 @@ echo "-------------------------------------------------------"
 
 # Install Docker Compose standalone binary
 echo "[+] Installing Docker Compose standalone binary..."
-LATEST_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-curl -L "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+if [ ! -x /usr/local/bin/docker-compose ]; then
+        LATEST_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+        curl -L "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
+    else
+        echo "docker-compose already installed"
+fi
 
 # Move to the script's directory
 cd "$(dirname "$0")"
