e8478c98080fcde6252532258252a7c52a592301
LinuxUser255 <atnl@protonmailcom>
2025-04-06 21:16:33 -0400

    augmented the venv setup script

--- Src/setup-venv.sh
+++ Src/setup-venv.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # Create Python virtual environment and install dependencies
-printf "\e[1;31m[+] Setting up Python virtual environment...\e[0m\n"
+printf "\e[1;31m[+] Creating the Python virtual environment...\e[0m\n"
 python3 -m venv venv
+
+printf "\e[1;31m[+] Activating the Python virtual environment...\e[0m\n"
 # sleep for 3 seconds
 sleep 3
 source venv/bin/activate
