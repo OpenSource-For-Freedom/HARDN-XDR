ed73661c640103236e4532ea6dd89c31b62942ca
Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>
2025-06-30 09:50:59 -0400

    Update install.sh
    Signed-off-by: Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-
+# HARDN-XDR installed entry point
 # Author: Christopher Bingham
-
 check_root () {
-        [ "$(id -u)" -ne 0 ] && echo "Please run this script as root." && exit 1
+    [ "$(id -u)" -ne 0 ] && echo "Please run this script as root." && exit 1
 }
-
 update_system() {
-        printf "\033[1;31m[+] Updating system...\033[0m\n"
-        apt update && apt upgrade -y
+    printf "\033[1;31m[+] Updating system...\033[0m\n"
+    apt update && apt upgrade -y
 }
-
-# Git clone the repo, cd into it, and run the script hardn-main.sh
-retrieve_and_run() {
-        git clone https://github.com/OpenSource-For-Freedom/HARDN-XDR
-        cd HARDN-XDR/src/setup &&  chmod +x hardn-main.sh && sudo ./hardn-main.sh
+run_hardn() {
+    MAIN_SCRIPT="/usr/share/hardn-xdr/src/setup/hardn-main.sh"
+    if [ -f "$MAIN_SCRIPT" ]; then
+        chmod +x "$MAIN_SCRIPT"
+        "$MAIN_SCRIPT"
+    else
+        echo "Main script not found at $MAIN_SCRIPT"
+        exit 1
+    fi
 }
-
-
 main() {
-        check_root
-        update_system
-        retrieve_and_run
+    check_root
+    update_system
+    run_hardn
 }
-
 main
